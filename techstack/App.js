2802fb6c09530b99eb6f5cedcc7e5de6cf5b1e24
hakbar0 <haseeb2000@hotmail.com>
2018-12-04 21:51:51 +0000

    Setup central state

--- techstack/App.js
+++ techstack/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers';
-import { Header } from './components/common';
+import reducers from './src/reducers';
+import { Header } from './src/components/common';
+import LibraryList from './src/reducers/LibraryList';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={createStore(reducers)}>
-        <View>
-          <Header headerText="Tech Stack" />
-        </View>
-      </Provider>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+};
 
 export default App;
